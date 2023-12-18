d1c47b59d127c9da7249cb8d24b78aee214b7aca
atchopba <atchopba@gmail.com>
2023-12-18 22:25:00 +0100

    Add function to send pdf document

--- whatsapp_data.php
+++ whatsapp_data.php
@@ -154,4 +154,93 @@ function build_data_with_image_and_no_dest_name($template_name, $business_name, 
     return $data;
 }
 
+function build_data_with_document_and_dest_name($template_name, $business_name, $recipient_phone_number, $dest_name, $message_text, $media) {
+    $data = array(
+        "messaging_product" => "whatsapp",
+        "to" => $recipient_phone_number,
+        "type" => "template",
+        "template" => array(
+            "name" => $template_name,
+            "language" => array(
+                "code" => "en_US",
+                "policy" => "deterministic"
+            ),
+            "components" => [
+                array(
+                    "type" => "header",
+                    "parameters" => [
+                        array(
+                            "type" => "document",
+                            "document" => array(
+                                "link" => $media  # "https://www.educatout.com/images/medium/Lenfant-qui-veut-controler-les-autres-FB.jpg"  # noqa
+                            )
+                        )
+                    ]
+                ),
+                array(
+                    "type" => "body",
+                    "parameters" => [
+                        array(
+                            "type" => "text",
+                            "text" => $dest_name
+                        ),
+                        array(
+                            "type" => "text",
+                            "text" => $message_text
+                        ),
+                        array(
+                            "type" => "text",
+                            "text" => $business_name
+                        )
+                    ]
+                )
+            ]
+        )
+    );
+    return $data;
+}    
+
+
+function build_data_with_document_and_no_dest_name($template_name, $business_name, $recipient_phone_number, $message_text, $media) {
+    $data = array(
+        "messaging_product" => "whatsapp",
+        "to" => $recipient_phone_number,
+        "type" => "template",
+        "template" => array(
+            "name" => $template_name,
+            "language" => array(
+                "code" => "en_US",
+                "policy" => "deterministic"
+            ),
+            "components" => [
+                array(
+                    "type" => "header",
+                    "parameters" => [
+                        array(
+                            "type" => "document",
+                            "document" => array(
+                                "link" => $media  # "https://www.educatout.com/images/medium/Lenfant-qui-veut-controler-les-autres-FB.jpg" # noqa
+                            )
+                        )
+                    ]
+                ),
+                array(
+                    "type" => "body",
+                    "parameters" => [
+                        array(
+                            "type" => "text",
+                            "text" => $message_text
+                        ),
+                        array(
+                            "type" => "text",
+                            "text" => $business_name
+                        )
+                    ]
+                )
+            ]
+        )
+    );
+    return $data;
+}
+
 ?>
