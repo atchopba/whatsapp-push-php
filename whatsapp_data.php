16541db6a06722b03eee65d11861b2ba3fc9ec3b
atchopba <atchopba@gmail.com>
2024-01-17 22:55:18 +0100

    Update whatsapp_data.php
    Add methods to send location, sms

--- whatsapp_data.php
+++ whatsapp_data.php
@@ -250,4 +250,60 @@ function build_data_with_document_and_no_dest_name($template_name, $business_nam
     return $data;
 }
 
+
+function build_data_with_buttons($template_name, $business_name, $recipient_phone_number) {
+    $data = array(
+        "messaging_product" => "whatsapp",
+        "to" => $recipient_phone_number,
+        "type" => "template",
+        "template" => array(
+            "name" => $template_name,
+            "language" => array(
+                "code" => "en_US"
+            ),
+            "components" => [
+                array(
+                    "type" => "body",
+                    "parameters" => [
+                        array(
+                            "type" => "text",
+                            "text" => $business_name
+                        )
+                    ]
+                )
+            ]
+        )
+    );
+    return $data;
+}
+
+
+function build_data_with_location($recipient_phone_number) {
+    $data = array(
+        "messaging_product" => "whatsapp",
+        "to" => $recipient_phone_number,
+        "type" => "location",
+        "location" => array(
+            "latitude" => 37.758056,
+            "longitude" => -122.425332,
+            "name" => "META HQ",
+            "address" => "1 Hacker Way, Menlo Park, CA 94025"
+        )
+    );
+    return $data;
+}
+
+
+function build_data_with_sms($recipient_phone_number, $message) {
+    $data = array(
+        "messaging_product" => "whatsapp",
+        "to" => $recipient_phone_number,
+        "type" => "text",
+        "text" => array(
+           "body" => $message
+        )
+    );
+    return $data;
+}
+
 ?>
