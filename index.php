c2c6150324b8261e9f9504d04622ffccacbe4657
atchopba <atchopba@gmail.com>
2024-03-18 22:20:09 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
 @include_once("./whatsapp_data.php");
+@include_once("./helpers.php");
 @include_once("./vars.inc.php");
 
 $API_VERSION = "v17.0";
@@ -20,30 +21,36 @@ $headers = array(
     "Content-Type: application/json"
 );
 
-$data_to_send = json_encode(build_data_with_image_and_dest_name($template_name, $business_name, $recipient_phone_number, $dest_name, $message_text, $media));
+$data_to_send = json_encode(build_data_for_image_and_dest_name($template_name, $business_name, $recipient_phone_number, $dest_name, $message_text, $media));
 
 $media = "https://www.erudit.org/fr/revues/ltp/1958-v14-n1-ltp0952/1019961ar.pdf";
+$image_link = "https://www.educatout.com/images/medium/Lenfant-qui-veut-controler-les-autres-FB.jpg";
 $template_name = "test_template_with_document_and_dest_name";
-$data_to_send = json_encode(build_data_with_document_and_dest_name($template_name, $business_name, $recipient_phone_number, $dest_name, $message_text, $media));
-$data_to_send = json_encode(build_data_with_buttons('test_template_with_nomessage_and_1button', $business_name, $recipient_phone_number));
-$data_to_send = json_encode(build_data_with_location($business_name, $recipient_phone_number));
+$data_to_send = json_encode(build_data_for_document_and_dest_name($template_name, $business_name, $recipient_phone_number, $dest_name, $message_text, $media));
+$data_to_send = json_encode(build_data_for_buttons('test_template_with_nomessage_and_1button', $business_name, $recipient_phone_number));
+$data_to_send = json_encode(build_data_for_location($business_name, $recipient_phone_number));
 
 $message_text = "message simple sans template";
-$data_to_send = json_encode(build_data_with_sms($recipient_phone_number, $message_text));
+$data_to_send = json_encode(build_data_for_sms($recipient_phone_number, $message_text));
+$data_to_send = json_encode(build_data_for_sms_send_your_location($recipient_phone_number, $message_text));
+$data_to_send = json_encode(build_data_for_yesno_sms($recipient_phone_number, $message_text));
+$data_to_send = json_encode(build_push_poll_message($recipient_phone_number, $message_text, null));
+$data_to_send = json_encode(build_data_for_sms_with_image($recipient_phone_number, $image_link));
+$data_to_send = json_encode(build_data_for_sms_with_media($recipient_phone_number, $image_link));
 
 echo "$url <br/>$data_to_send";
  
 $curl = curl_init($url);
 curl_setopt($curl, CURLOPT_URL, $url);
 curl_setopt($curl, CURLOPT_POST, 1);
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, false);
 
 curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($curl, CURLOPT_POSTFIELDS, $data_to_send);
 
-echo "<br>result: <br>";
+echo "<br>execution: <br>";
 $resp = curl_exec($curl);
-echo $resp;
+echo "<br><br/>result: ". $resp;
 curl_close($curl);
 
 ?>
