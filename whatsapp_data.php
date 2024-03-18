bf42e1143ff5bb0b20c6013d68bc03eb776a9626
atchopba <atchopba@gmail.com>
2024-03-18 22:19:10 +0100

    Update whatsapp_data.php

--- whatsapp_data.php
+++ whatsapp_data.php
@@ -35,7 +35,7 @@ function build_data_without_dest_name($template_name, $business_name, $recipient
     return $data;
 }
 
-function build_data_with_dest_name($template_name, $business_name, $recipient_phone_number, $dest_name, $message_text) {
+function build_data_for_dest_name($template_name, $business_name, $recipient_phone_number, $dest_name, $message_text) {
     $data = array(
         "messaging_product" => "whatsapp",
         "to" => $recipient_phone_number,
@@ -70,7 +70,7 @@ function build_data_with_dest_name($template_name, $business_name, $recipient_ph
 }
 
 
-function build_data_with_image_and_dest_name($template_name, $business_name, $recipient_phone_number, $dest_name, $message_text, $media) {
+function build_data_for_image_and_dest_name($template_name, $business_name, $recipient_phone_number, $dest_name, $message_text, $media) {
     $data = array(
         "messaging_product" => "whatsapp",
         "to" => $recipient_phone_number,
@@ -117,7 +117,7 @@ function build_data_with_image_and_dest_name($template_name, $business_name, $re
 }    
 
 
-function build_data_with_image_and_no_dest_name($template_name, $business_name, $recipient_phone_number, $message_text, $media) {
+function build_data_for_image_and_no_dest_name($template_name, $business_name, $recipient_phone_number, $message_text, $media) {
     $data = array(
         "messaging_product" => "whatsapp",
         "to" => $recipient_phone_number,
@@ -159,7 +159,7 @@ function build_data_with_image_and_no_dest_name($template_name, $business_name, 
     return $data;
 }
 
-function build_data_with_document_and_dest_name($template_name, $business_name, $recipient_phone_number, $dest_name, $message_text, $media) {
+function build_data_for_document_and_dest_name($template_name, $business_name, $recipient_phone_number, $dest_name, $message_text, $media) {
     $data = array(
         "messaging_product" => "whatsapp",
         "to" => $recipient_phone_number,
@@ -207,7 +207,7 @@ function build_data_with_document_and_dest_name($template_name, $business_name, 
 }    
 
 
-function build_data_with_document_and_no_dest_name($template_name, $business_name, $recipient_phone_number, $message_text, $media) {
+function build_data_for_document_and_no_dest_name($template_name, $business_name, $recipient_phone_number, $message_text, $media) {
     $data = array(
         "messaging_product" => "whatsapp",
         "to" => $recipient_phone_number,
@@ -251,7 +251,7 @@ function build_data_with_document_and_no_dest_name($template_name, $business_nam
 }
 
 
-function build_data_with_buttons($template_name, $business_name, $recipient_phone_number) {
+function build_data_for_buttons($template_name, $business_name, $recipient_phone_number) {
     $data = array(
         "messaging_product" => "whatsapp",
         "to" => $recipient_phone_number,
@@ -278,7 +278,7 @@ function build_data_with_buttons($template_name, $business_name, $recipient_phon
 }
 
 
-function build_data_with_location($recipient_phone_number) {
+function build_data_for_location($recipient_phone_number) {
     $data = array(
         "messaging_product" => "whatsapp",
         "to" => $recipient_phone_number,
@@ -294,16 +294,134 @@ function build_data_with_location($recipient_phone_number) {
 }
 
 
-function build_data_with_sms($recipient_phone_number, $message) {
+function build_data_for_sms($recipient_phone_number, $message) {
     $data = array(
         "messaging_product" => "whatsapp",
+        "recipient_type" => "individual",
         "to" => $recipient_phone_number,
         "type" => "text",
         "text" => array(
-           "body" => $message
+            "body" => $message
         )
     );
     return $data;
 }
 
+
+function build_data_for_sms_with_image($recipient_phone_number, $image_link) {
+    $data = array(
+        "messaging_product" => "whatsapp",
+        "to" => $recipient_phone_number,
+        "type" => "image",
+        "image" => array(
+            "link" => $image_link
+        )
+    );
+    return $data;
+}
+
+
+function build_data_for_sms_with_media($recipient_phone_number, $media_link) {
+    $media = get_media($media_link);
+    print_r($media);
+    $data = array(
+        "messaging_product" => "whatsapp",
+        "to" => $recipient_phone_number
+    );
+    $data = array_merge($data, $media);
+    return $data;
+}
+
+
+function build_data_for_yesno_sms($recipient_phone_number, $message) {
+    $data = array(
+        "messaging_product" => "whatsapp",
+        "to" => $recipient_phone_number,
+        "type" => "interactive",
+        "interactive" => array(
+            "type" => "button",
+            "body" => array(
+                "text" => $message . "\n\nPlease respond with 'Yes' or 'No'"
+            ),
+            "action" => array(
+                "buttons" => [
+                    array(
+                        "type" => "reply",
+                        "reply" => array(
+                            "id" => "unique-postback-yes",
+                            "title" => "Yes"
+                        )
+                    ),
+                    array(
+                        "type" => "reply",
+                        "reply" => array(
+                            "id" => "unique-postback-no",
+                            "title" => "No"
+                        )
+                    )
+                ]
+            )
+        )
+    );
+    return $data;
+}
+
+
+function build_data_for_sms_send_your_location($recipient_phone_number, $message) {
+    $data = array(
+        "messaging_product" => "whatsapp",
+        "to" => $recipient_phone_number,
+        "type" => "interactive",
+        "interactive" => array(
+            "type" => "location_request_message",
+            "body" => array(
+                "text" => $message
+            ),
+            "action" => array(
+                "name" => "send_location"
+            )
+        )
+    );
+    return $data;
+}
+
+function build_push_poll_message($recipient_phone_number, $message, $items) {
+    $data = array(
+        "messaging_product" => "whatsapp",
+        "to" => $recipient_phone_number,
+        "type" => "interactive",
+        "interactive" => array(
+            "type" => "list",
+            "body" => array(
+                "text" => $message . "\n\nPlease respond with 'Yes' or 'No'"
+            ),
+            "action" => array(
+                "button" => "Faîtes votre choix: ",
+                "sections" => [
+                    array(
+                        "title" => "Faîtes votre choix: ",
+                        "rows" => [
+                            array(
+                                "id" => "id1",
+                                "title" => "title1",
+                                "description" => "description1"
+                            ),
+                            array(
+                                "id" => "id2",
+                                "title" => "title2",
+                                "description" => "description2"
+                            ),
+                            array(
+                                "id" => "id3",
+                                "title" => "title3",
+                                "description" => "description3"
+                            ),
+                        ]
+                    )
+                ]
+            )
+        )
+    );
+    return $data;
+}
 ?>
